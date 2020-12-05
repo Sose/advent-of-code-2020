5a4105a5050bed9169ea2ced5a3d9085e6697c2c
Joni Hiltunen <djonih@gmail.com>
2020-12-05 10:05:58 +0200

    Better style

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,75 +1,63 @@
 module Day02 where
 
 import           Control.Applicative            ( some )
+import           Data.Maybe                     ( fromJust )
 
 import           Lib
-import qualified Parser                        as P
+import           Parser
 
-type MinOcc = Int
-type MaxOcc = Int
-data Policy = Policy MinOcc MaxOcc Char
+data Policy = Policy Int Int Char
     deriving (Eq, Show)
 
-type Password = String
-data DBEntry = DBEntry Policy Password
+data DBEntry = DBEntry Policy String
     deriving (Eq, Show)
 
 parseEntry :: String -> Maybe DBEntry
-parseEntry s = case P.runParser entryParser s of
+parseEntry s = case runParser entryParser s of
     [(x, "")] -> Just x
     _         -> Nothing
 
-entryParser :: P.Parser DBEntry
-entryParser = do
-    min  <- P.posInt
-    _    <- P.char '-'
-    max  <- P.posInt
-    _    <- P.space
-    c    <- P.alpha
-    _    <- P.char ':'
-    _    <- P.space
-    pass <- some P.alpha
-    return $ DBEntry (Policy min max c) pass
+policy :: Parser Policy
+policy = Policy <$> posInt <* char '-' <*> posInt <* space <*> alpha
+
+password :: Parser String
+password = some alpha
+
+entryParser :: Parser DBEntry
+entryParser = DBEntry <$> policy <* char ':' <* space <*> password
 
 -- Count how many times a char occurs in a string
 countChars :: Char -> String -> Int
-countChars c s = length $ filter (== c) s
+countChars c s = length (filter (== c) s)
 
 -- Validate a single DBEntry according to part 1
 entryIsValidP1 :: DBEntry -> Bool
 entryIsValidP1 (DBEntry (Policy minc maxc c) pass) =
-    let cOccurs = countChars c pass in minc <= cOccurs && cOccurs <= maxc
+    minc <= cOccurs && cOccurs <= maxc
+    where cOccurs = countChars c pass
 
 -- Part 2 rules
 entryIsValidP2 :: DBEntry -> Bool
 entryIsValidP2 (DBEntry (Policy p1 p2 c) pass) =
-    let pos1 = p1 - 1 -- positions start from 1 instead of 0
-        pos2 = p2 - 1
-    in  (length pass > pos1 && length pass > pos2)
-            && (  (pass !! pos1 == c && pass !! pos2 /= c)
-               || (pass !! pos1 /= c && pass !! pos2 == c)
-               )
+    (length pass > pos1 && length pass > pos2)
+        && (  (pass !! pos1 == c && pass !! pos2 /= c)
+           || (pass !! pos1 /= c && pass !! pos2 == c)
+           )
+  where
+    pos1 = p1 - 1 -- positions start from 1 instead of 0
+    pos2 = p2 - 1
 
--- part1 takes a list of Strings like "1-3 a: abcde" and counts how many
--- entries are valid
-doPart :: (DBEntry -> Bool) -> [String] -> Either String Int
-doPart partFn strs = case sequence $ parseEntry <$> strs of
-    Nothing   -> Left "Error while parsing input"
-    (Just es) -> Right $ length $ filter (== True) $ partFn <$> es
-
-part1 :: [String] -> Either String Int
-part1 = doPart entryIsValidP1
-
-part2 :: [String] -> Either String Int
-part2 = doPart entryIsValidP2
+doPart :: (DBEntry -> Bool) -> [DBEntry] -> Int
+doPart partFn es = length (filter id (partFn <$> es))
 
 --- Read input and calculate both p1 and p2 and print the results
 
 main :: IO ()
 main = do
-    ls <- readLines "02"
-    let p1 = part1 ls
-    let p2 = part2 ls
     putStrLn "Day 02"
+    ls <- readLines "02"
+    let entries = mapM parseEntry ls
+    let p1      = fromJust (doPart entryIsValidP1 <$> entries)
+    let p2      = fromJust (doPart entryIsValidP2 <$> entries)
     print p1
     print p2
